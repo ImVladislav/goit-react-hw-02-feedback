ad2c3c16c0e3d89da0dd0936a4d1ce5e5a86e0a6
ImVladislav <nafanya102@gmail.com>
2022-11-29 18:26:14 +0200

    23

---
+++ src/components/FeedbackCounter/FeedbackCounter.js
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyledSection } from './FeedBackCounter.styled';
+import { Title } from 'components/Title/Title';
+
+export class Section extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+
+
+    return (
+      <StyledSection>
+        <Title title={this.props.title} />
+      </StyledSection>
+    );
+  }
+}
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
